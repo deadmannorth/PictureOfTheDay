36dc3f92d446385a360f7c6c8c0156f888966b3b
”Alexey <A_Goth@mail.ru>
2021-07-10 20:35:52 +0300

    complete load

---
+++ app/src/main/java/ru/aslazarev/pictureoftheday/ui/picture/PictureOfTheDayFragment.kt
@@ -0,0 +1,137 @@
+package ru.aslazarev.pictureoftheday.ui.picture
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import coil.api.load
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import ru.aslazarev.pictureoftheday.R
+import ru.aslazarev.pictureoftheday.databinding.FragmentPictureOfTheDayBinding
+import ru.aslazarev.pictureoftheday.ui.MainActivity
+import ru.aslazarev.pictureoftheday.ui.chips.ChipsFragment
+
+class PictureOfTheDayFragment : Fragment() {
+
+    private lateinit var binding: FragmentPictureOfTheDayBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding){
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getData()
+            .observe(viewLifecycleOwner, { renderData(it, view) })
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        inputLayout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${inputEditText.text.toString()}")
+            })
+        }
+        setBottomAppBar(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> toast("Favourite")
+            R.id.app_bar_settings -> activity?.supportFragmentManager?.beginTransaction()?.add(R.id.container, ChipsFragment())?.addToBackStack(null)?.commit()
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun renderData(data: PictureOfTheDayData, view: View) = with(binding) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                val title = view.findViewById<TextView>(R.id.bottom_sheet_description_header)
+                val explanation = view.findViewById<TextView>(R.id.bottom_sheet_description)
+                if (url.isNullOrEmpty()) {
+                    //showError("Сообщение, что ссылка пустая")
+                    toast("Link is empty")
+                } else {
+                    //showSuccess()
+                    imageView.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
+                    }
+                    title.text = serverResponseData.title
+                    explanation.text = serverResponseData.explanation
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+                //showError(data.error.message)
+                toast(data.error.message)
+            }
+        }
+    }
+
+    private fun setBottomAppBar(view: View) = with(binding){
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+        fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                bottomAppBar.navigationIcon = null
+                bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_back_fab))
+                bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(context, R.drawable.ic_hamburger_menu_bottom_bar)
+                bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_plus_fab))
+                bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
+    }
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+        private var isMain = true
+    }
+}
